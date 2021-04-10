ce15b9acfacf26afb53fc7c27c543eab70546789
Felipe Cardozo <fraifelipe@gmail.com>
2021-04-09 22:46:59 -0300

    update template

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+type eita = 'epa' | 'opa'
+
+
+
+let a: eita = 'epa';
+
+if (a == 'epa') {
+    console.log('hello vish');
+} else {
+    console.log('hello vish 2');
+}
+
+
+console.log(eitao(3))
+
+
+function eitao(x: number) {
+    return x * 42
+}
